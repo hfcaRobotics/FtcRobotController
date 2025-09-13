ad47b7a2e3dc18913d0f842b2776054a1065b36c
hfcaRobotics <robotics@hfca.org>
2025-09-13 12:53:42 -0500

    Gamepad Test
    September 13th, 2025

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadTest.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-public class HelloWorld extends OpMode {
+public class GamepadTest extends OpMode {
 
     @Override
     public void init() {
@@ -13,6 +13,8 @@ public class HelloWorld extends OpMode {
 
     @Override
     public void loop() {
-
+        telemetry.addData("x", gamepad1.left_stick_x);
+        telemetry.addData("y", gamepad1.left_stick_y);
+        telemetry.addData("a", gamepad1.a);
     }
 }
