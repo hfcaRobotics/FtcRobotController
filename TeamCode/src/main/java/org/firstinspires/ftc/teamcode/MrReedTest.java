20dd366b29b482da9455dcd32caef8fc36489571
richaadreed <richaad.reed@gmail.com>
2025-09-16 21:29:16 -0500

    Create MrReedTest.java
    Coach Reeds Frist Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MrReedTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous
+public class MrReedTest extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Mr Reed", "Is HILARIOUS"); //How to make a comment
+    }
+
+    @Override
+    public void loop() {
+
+    }
+//single line comment
+    /*
+    multi-line comment
+     */
+
+}
