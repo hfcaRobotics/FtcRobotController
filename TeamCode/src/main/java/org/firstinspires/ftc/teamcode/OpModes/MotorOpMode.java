5e5718df232de574474159ea05c401d37b923237
hfcaRobotics <robotics@hfca.org>
2025-09-20 11:29:03 -0500

    Motor test
    We got the motor to run

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MotorOpMode.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanism.MotorHardware;
+
+@TeleOp
+public class MotorOpMode extends OpMode {
+    MotorHardware board = new MotorHardware();
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+    @Override
+    public void loop(){
+        double motorSpeed = -gamepad1.left_stick_y/2 * gamepad1.left_trigger;
+        board.setMotorSpeed(motorSpeed);
+        //board.setMotorSpeed(-gamepad1.left_trigger);
+        //board.setMotorSpeed(gamepad1.right_trigger);
+    }
+}
