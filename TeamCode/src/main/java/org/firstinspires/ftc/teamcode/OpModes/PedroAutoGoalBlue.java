7b58f3d05611b049ffc1d4d026fd782118db074a
Cometfable <aoforianim0806@gmail.com>
2026-02-14 11:08:46 -0600

    Red Launch Change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PedroAutoGoalBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PedroAutoGoalBlue.java
@@ -50,8 +50,8 @@ public class PedroAutoGoalBlue extends OpMode
      * velocity. Here we are setting the target and minimum velocity that the launcher should run
      * at. The minimum velocity is a threshold for determining when to fire.
      */
-    final double LAUNCHER_TARGET_VELOCITY = 1125;
-    final double LAUNCHER_MIN_VELOCITY = 1075;
+    final double LAUNCHER_TARGET_VELOCITY = 1050;
+    final double LAUNCHER_MIN_VELOCITY = 1000;
 
     /*
      * The number of seconds that we wait between each of our 3 shots from the launcher. This
